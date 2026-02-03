0b327c702a0a91c9a312e3c3171e398dfebe5cd1
stewens4life-dot <stewens4life@gmail.com>
2026-02-03 12:34:03 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <--- Agrega esta línea
-  ],
+  plugins: [react()],
+  server: {
+    host: true, // Esto permite acceso desde la red (IP)
+    port: 5173  // Puerto fijo opcional
+  }
 })
